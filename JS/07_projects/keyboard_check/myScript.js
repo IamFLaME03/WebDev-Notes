6e269e606fbc2562049a420de04711841c12140a
Kirtan Patel <134549574+IamFLaME03@users.noreply.github.com>
2024-09-01 14:54:46 +0530

    project keyboard check complete

---
+++ JS/07_projects/keyboard_check/myScript.js
@@ -0,0 +1,21 @@
+const main = document.querySelector('main');
+
+window.addEventListener('keydown', (e) => {
+  main.innerHTML = `
+    <div class='color'>
+    <table>
+    <tr>
+      <th>Key</th>
+      <th>Keycode</th> 
+      <th>Code</th>
+    </tr>
+    <tr>
+      <td>${e.key === ' ' ? 'Space' : e.key}</td>
+      <td>${e.keyCode}</td> 
+      <td>${e.code}</td>
+    </tr>
+    
+  </table>
+    </div>
+  `;
+});
