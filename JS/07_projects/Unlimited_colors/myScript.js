3e3841be1f8c5618a0b2ac3443dad041e2e5ffea
Kirtan Patel <134549574+IamFLaME03@users.noreply.github.com>
2024-09-01 15:09:37 +0530

    project unlimited colors complete

--- JS/07_projects/Unlimited_colors/myScript.js
+++ JS/07_projects/Unlimited_colors/myScript.js
@@ -1,6 +1,24 @@
-const box = document.querySelector('.tbox')
+const start = document.querySelector("#start")
+const stop = document.querySelector("#stop")
 
-setInterval(function(){
-    box.innerHTML = new Date().toLocaleTimeString()
-}, 1000);
 
+let intervalId;
+start.addEventListener("click", () => {
+  if (!intervalId) {
+    intervalId = setInterval(changeBgColor, 500);
+  }
+
+  function changeBgColor() {
+    const hex = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += hex[Math.floor(Math.random() * 16)];
+    }
+    document.body.style.backgroundColor = color;
+  }
+});
+
+stop.addEventListener("click", () => {
+  clearInterval(intervalId);
+  intervalId = null;
+});
