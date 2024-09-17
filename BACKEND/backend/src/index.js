18625f6b4de50e68a9ea2575e2ffa2c5956f0f24
Kirtan Patel <134549574+IamFLaME03@users.noreply.github.com>
2024-09-17 12:21:34 +0530

    connect to mongo atlas

---
+++ BACKEND/backend/src/index.js
@@ -0,0 +1,33 @@
+// require('dotenv').config({path: './env'}) -> consistancy problem
+import dotenv from 'dotenv'
+import connectDB from "./db/index.js";
+
+dotenv.config({ 
+    path:'./env'
+})
+
+connectDB()
+
+/* Approach - 1 
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+import express from "express"
+const app = express()
+(async () => {
+    try{
+        mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", ()=>{
+            console.log("ERR: ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+    }
+    catch (error){
+        console.error("ERROR: ", error)
+        throw err
+    }
+})()
+*/
